8c63127af2600fd91b82b10049d35006a5744bdf
MengAaron <871628893@qq.com>
2021-05-28 17:23:56 +0800

    fix

--- pcdet/models/backbones_range/RangeRCNN_extractor.py
+++ pcdet/models/backbones_range/RangeRCNN_extractor.py
@@ -12,15 +12,15 @@ class DRBlock(nn.Module):
         self.out_channels = out_channels
         # residual function
         self.conv0 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding='SAME', bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True))
         self.conv1 = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False, dilation=2),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding='SAME', bias=False, dilation=2),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True))
         self.conv2 = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False, dilation=3),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding='SAME', bias=False, dilation=3),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True))
         self.conv_fuse = nn.Sequential(
@@ -72,7 +72,7 @@ class RangeRCNNBackbone(nn.Module):
         self.Down3 = DDBlock(128, 128)
         self.Down4 = DDBlock(128, 256)
         self.Down5 = DDBlock(256, 256)
-        self.Up4 = DRBlock(512,128)
+        self.Up4 = DRBlock(512, 128)
         self.Up3 = DRBlock(256, 128)
         self.Up2 = DRBlock(256, 64)
         self.Up1 = DRBlock(128, 64)
@@ -87,13 +87,13 @@ class RangeRCNNBackbone(nn.Module):
         conv3 = self.Down3(conv2)
         conv4 = self.Down4(conv3)
         conv5 = self.Down5(conv4)
-        up4 = torch.cat([nn.UpsamplingBilinear2d(conv5),conv4],dim=1)
+        up4 = torch.cat([nn.UpsamplingBilinear2d(conv5), conv4], dim=1)
         up4 = self.Up4(up4)
-        up3 = torch.cat([nn.UpsamplingBilinear2d(up4),conv3],dim=1)
+        up3 = torch.cat([nn.UpsamplingBilinear2d(up4), conv3], dim=1)
         up3 = self.Up4(up3)
-        up2 = torch.cat([nn.UpsamplingBilinear2d(up3),conv2],dim=1)
+        up2 = torch.cat([nn.UpsamplingBilinear2d(up3), conv2], dim=1)
         up2 = self.Up4(up2)
-        up1 = torch.cat([nn.UpsamplingBilinear2d(up2),conv1],dim=1)
+        up1 = torch.cat([nn.UpsamplingBilinear2d(up2), conv1], dim=1)
         up1 = self.Up4(up1)
         batch_dict.pop('range_image', None)
         batch_dict['range_features'] = up1

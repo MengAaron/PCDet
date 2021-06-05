b12a3430bd9f58a56843446caf002ae8ba313e4d
MengAaron <871628893@qq.com>
2021-06-05 16:27:56 +0800

    fix

--- pcdet/models/backbones_range/__init__.py
+++ pcdet/models/backbones_range/__init__.py
@@ -1,9 +1,13 @@
 # range image feature extractor
 from .rsn_extractor import UNet,UNet2
 from .RangeRCNN_extractor import RangeRCNNBackbone
+from .resnet import ResNet,ResNetV1c,ResNetV1d
 
 __all__ = {
     'UNet': UNet,
     'UNet2': UNet2,
     'RangeRCNNBackbone':RangeRCNNBackbone,
+    'ResNet': ResNet,
+    'ResNetV1c': ResNetV1c,
+    'ResNetV1d': ResNetV1d
 }

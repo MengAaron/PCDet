9bbce8f644c556b00d4256dc8050bf5c4ec78e00
MengAaron <871628893@qq.com>
2021-06-05 20:25:37 +0800

    fix

--- pcdet/models/seg_heads/nl_head.py
+++ pcdet/models/seg_heads/nl_head.py
@@ -333,9 +333,10 @@ class FCNHead(nn.Module):
         self.num_convs = num_convs
         self.concat_input = concat_input
         self.kernel_size = kernel_size
+        self.channels = model_cfg.channels
         self.conv_seg = nn.Conv2d(self.channels, 1, kernel_size=1)
         # self.in_channels = in_channels
-        self.channels = model_cfg.channels
+
         self.norm_cfg = norm_cfg
         if dropout_ratio > 0:
             self.dropout = nn.Dropout2d(dropout_ratio)

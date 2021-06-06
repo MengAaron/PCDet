8f77b044907e4de87c25bfca3c2caf200870c0e0
MengAaron <871628893@qq.com>
2021-06-06 16:06:52 +0800

    fix

--- pcdet/datasets/waymo/waymo_dataset.py
+++ pcdet/datasets/waymo/waymo_dataset.py
@@ -183,7 +183,7 @@ class WaymoDataset(DatasetTemplate):
             data_dict = self.prepare_data(data_dict=input_dict, process=False)
         else:
             data_dict = self.prepare_data(data_dict=input_dict)
-        # data_dict['metadata'] = info.get('metadata', info['frame_id'])
+        data_dict['metadata'] = info.get('metadata', info['frame_id'])
         data_dict.pop('num_points_in_gt', None)
 
         if not self.training and self.dataset_cfg.get('USE_DOUBLE_FLIP_TEST', False):

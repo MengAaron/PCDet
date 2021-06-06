4ca2468e2e7f466cd1d4846abaf504123a332e1f
MengAaron <871628893@qq.com>
2021-06-06 23:06:10 +0800

    fix

--- pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
+++ pcdet/models/seg_heads/map_to_point_cloud/point_gather.py
@@ -58,7 +58,7 @@ class PointGather(nn.Module):
                 cur_seg_mask = seg_mask[batch_idx] >= self.foreground_threshold
             else:
                 # first feature map is foreground
-                # cur_seg_mask = (seg_mask[batch_idx].argmax(dim=0).bool())
+                cur_seg_mask = (seg_mask[batch_idx].argmax(dim=0).bool())
                 cur_seg_mask = seg_mask[batch_idx][1] >= self.foreground_threshold
 
             cur_seg_mask = torch.flatten(cur_seg_mask)
